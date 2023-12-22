58421e79b4ba065841fb36fece7681ae6d8440ee
Josue Figueroa <poor.rican@pm.me>
2023-12-22 15:27:50 -0500

    Implement conversion of Vec<Candle> to DataFrame

--- src/types/candles.rs
+++ src/types/candles.rs
@@ -57,6 +57,35 @@ impl AsDataFrame for Candle {
     }
 }
 
+impl AsDataFrame for Vec<Candle> {
+    fn as_dataframe(&self) -> DataFrame {
+        let mut time = Vec::with_capacity(self.len());
+        let mut open = Vec::with_capacity(self.len());
+        let mut high = Vec::with_capacity(self.len());
+        let mut low = Vec::with_capacity(self.len());
+        let mut close = Vec::with_capacity(self.len());
+        let mut volume = Vec::with_capacity(self.len());
+
+        for candle in self {
+            time.push(candle.time);
+            open.push(candle.open);
+            high.push(candle.high);
+            low.push(candle.low);
+            close.push(candle.close);
+            volume.push(candle.volume);
+        }
+
+        DataFrame::new(vec![
+            Series::new("time", time),
+            Series::new("open", open),
+            Series::new("high", high),
+            Series::new("low", low),
+            Series::new("close", close),
+            Series::new("volume", volume),
+        ]).unwrap()
+    }
+}
+
 
 #[cfg(test)]
 mod test {
@@ -95,5 +124,45 @@ mod test {
         assert_eq!(
             df.column("volume").unwrap().get(0).unwrap(),
             AnyValue::Float64(candle.volume));
+    }
+
+    #[test]
+    fn test_vec_of_candles_to_dataframe() {
+        // generate some candles
+        let mut candles = Vec::new();
+        for i in 0..10 {
+            let time = NaiveDateTime::from_timestamp_opt(Utc::now().timestamp() + i, 0).unwrap();
+            let candle = Candle {
+                time,
+                open: 1.0 + i as f64,
+                high: 2.0 + i as f64,
+                low: 3.0 + i as f64,
+                close: 4.0 + i as f64,
+                volume: 5.0 + i as f64,
+            };
+            candles.push(candle);
+        }
+        let df = candles.as_dataframe();
+        assert_eq!(df.shape(), (10, 6));
+        assert_eq!(df.get_column_names(), &["time", "open", "high", "low", "close", "volume"]);
+        for i in 0..10 {
+            assert_eq!(df.column("time").unwrap().datetime().unwrap().get(i).unwrap(),
+                       candles[i].time.timestamp_millis());
+            assert_eq!(
+                df.column("open").unwrap().get(i).unwrap(),
+                AnyValue::Float64(candles[i].open));
+            assert_eq!(
+                df.column("high").unwrap().get(i).unwrap(),
+                AnyValue::Float64(candles[i].high));
+            assert_eq!(
+                df.column("low").unwrap().get(i).unwrap(),
+                AnyValue::Float64(candles[i].low));
+            assert_eq!(
+                df.column("close").unwrap().get(i).unwrap(),
+                AnyValue::Float64(candles[i].close));
+            assert_eq!(
+                df.column("volume").unwrap().get(i).unwrap(),
+                AnyValue::Float64(candles[i].volume));
+        }
     }
 }
