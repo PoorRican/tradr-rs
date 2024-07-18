b4e9d586943b6cf03544896dc449b10ea7b5a2a8
Josue Figueroa <poor.rican@pm.me>
2024-07-17 20:58:25 -0700

    `BacktestingRunner` should initialize `Portfolio` upon run #22

--- src/backtesting.rs
+++ src/backtesting.rs
@@ -1,43 +1,37 @@
 use chrono::{DateTime};
 use polars::prelude::*;
 
-use crate::portfolio::{PerformanceMetrics, Portfolio, PositionHandlers, TradeHandlers};
+use crate::portfolio::{PerformanceMetrics, Portfolio, PortfolioArgs, PositionHandlers, TradeHandlers};
 use crate::strategies::Strategy;
 use crate::types::{ExecutedTrade, FutureTrade, Side};
 use crate::utils;
 
 pub struct BacktestingRunner<'a> {
     strategy: Strategy,
-    portfolio: Portfolio,
-    historical_data: &'a DataFrame,
+    portfolio_args: PortfolioArgs,
 }
 
 impl<'a> BacktestingRunner<'a> {
-    pub fn new(
-        strategy: Strategy,
-        init_assets: f64,
-        init_capital: f64,
-        candles: &'a DataFrame,
-    ) -> Self {
-        let start_time = candles.column("time").unwrap().datetime().unwrap().get(0).unwrap();
-        let start_time = DateTime::from_timestamp_millis(start_time).unwrap().naive_utc();
-
-        let portfolio = Portfolio::new(init_assets, init_capital, start_time);
+    pub fn new(strategy: Strategy, portfolio_args: PortfolioArgs) -> Self {
         BacktestingRunner {
-            portfolio,
-            historical_data: candles,
+            portfolio_args,
             strategy
         }
     }
 
-    pub fn run(&mut self) -> Result<PerformanceMetrics, ()> {
+    pub fn run(&mut self, candles: &DataFrame) -> Result<PerformanceMetrics, ()> {
         // process historical data
-        self.strategy.bootstrap(self.historical_data);
+        self.strategy.bootstrap(candles);
+
+        // initialize portfolio
+        let start_time = candles.column("time").unwrap().datetime().unwrap().get(0).unwrap();
+        let start_time = DateTime::from_timestamp_millis(start_time).unwrap().naive_utc();
+        let mut portfolio = Portfolio::from_args(&self.portfolio_args, start_time);
 
         if let Ok(signals) = self.strategy.get_combined_signals() {
             if let Some(signals) = signals {
                 // join signals with historical data
-                let combined = self.historical_data
+                let combined = candles
                     .clone()
                     .lazy()
                     .join(
@@ -62,24 +56,24 @@ impl<'a> BacktestingRunner<'a> {
                 // trading logic
                 for (candle, side) in candles.iter().zip(sides.iter()) {
                     let trade = match side {
-                        Side::Buy => self.portfolio.generate_buy_opt(&candle),
-                        Side::Sell => self.portfolio.generate_sell_opt(&candle)
+                        Side::Buy => portfolio.generate_buy_opt(&candle),
+                        Side::Sell => portfolio.generate_sell_opt(&candle)
                     };
 
                     // attempt trades
                     // TODO: simulate market conditions by adding randomness
                     if let Some(trade) = trade {
                         let executed = ExecutedTrade::with_future_trade("".to_string(), trade);
-                        self.portfolio.add_executed_trade(executed);
+                        portfolio.add_executed_trade(executed);
                     }
                 }
 
-                println!("Trades: {:?}", self.portfolio.get_executed_trades());
+                println!("Trades: {:?}", portfolio.get_executed_trades());
 
-                println!("Open Positions: {:?}", self.portfolio.get_open_positions().unwrap());
+                println!("Open Positions: {:?}", portfolio.get_open_positions().unwrap());
 
                 // arbitrarily using a 3% risk-free rate
-                Ok(self.portfolio.calculate_performance_metrics(0.03).unwrap())
+                Ok(portfolio.calculate_performance_metrics(0.03).unwrap())
             } else {
                 // TODO: return err for no indicators
                 todo!()
