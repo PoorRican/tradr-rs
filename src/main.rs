fa989f84b0633eefc9ef326f0194514380b14c1a
Josue Figueroa <poor.rican@pm.me>
2024-07-17 22:40:04 -0700

    Formalize `main.rs`

--- src/main.rs
+++ src/main.rs
@@ -1,74 +1,41 @@
-use std::path::Path;
-use std::time::Duration;
-use tokio::time::sleep;
-use crate::timing::wait_until;
+use crate::traits::AsDataFrame;
+use polars::prelude::*;
+use crate::backtesting::BacktestingRunner;
+use crate::portfolio::PortfolioArgs;
 
+mod backtesting;
 mod indicators;
 mod markets;
 mod portfolio;
-mod strategies;
 mod serialization;
+mod strategies;
+mod timing;
 mod traits;
 mod types;
-mod engine;
 mod utils;
 mod timing;
 
+fn main() {
+    let db_path = "data/candle_data.sqlite3";
+    let table_name = "coinbase_SHIBUSD_1m_candles";
 
-#[tokio::main]
-async fn main() {
-    println!("Starting...\n");
+    let candles = utils::extract_candles_from_db(db_path, table_name).unwrap().as_dataframe();
 
-    const INTERVAL: &str = "5m";
+    let strategy = strategies::Strategy::new(
+        vec![Box::new(indicators::BBands::default())],
+        strategies::Consensus::Unison,
+    );
 
-    let market = markets::CoinbaseClient::new()
-        .disable_trades();
-    let portfolio = portfolio::Portfolio::new(0.0, 100.0, None);
-    let strategy = strategies::Strategy::new(vec![
-        Box::new(indicators::BBands::new(20, 2.0)),
-    ], strategies::Consensus::Unison);
-    let mut engine = engine::Engine::new(
-        INTERVAL,
-        portfolio,
+    let mut runner = BacktestingRunner::new(
         strategy,
-        "MATIC-USD",
-        &market);
+        PortfolioArgs {
+        assets: 0.0,
+        capital: 100.0,
+        threshold: 0.0,
+        ..Default::default()
+    });
 
+    let performance = runner.run(&candles).unwrap();
 
-    // setup path
-    let path = Path::new("data");
-
-    // bootstrap
-    engine.initialize().await;
-    engine.save(path)
-        .expect("Failed to save data");
-
-    // wait until the next candle is released
-    println!("Waiting until next interval...");
-    wait_until(INTERVAL).await;
-
-    // run the engine once per interval
-    println!("\nBeginning loop...");
-    print_time();
-
-    loop {
-        print!("\n");
-        // if there is no new data, wait then try again
-        // certain intervals might not have data available and can be significantly delayed.
-        // therefore, in order to catch as much data as possible, a wait time is necessary
-        // in order to prevent the next update from returning more than one row
-        while !engine.run().await {
-            let wait = 20;
-            sleep(Duration::from_secs(wait)).await;
-        }
-        println!("Ran 1 iteration");
-        print_time();
-        engine.save(path).unwrap();
-        wait_until(INTERVAL).await;
-    }
-}
-
-fn print_time() {
-    let time = chrono::Utc::now();
-    println!("The time is now {time}");
+    println!("Performance: {:?}", performance);
 }
